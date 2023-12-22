b1beb7fb1e4087be31a1dbab34da0c55e58fc251
Mustafa Tolga Akbaba <76005690+mtlgkbb20@users.noreply.github.com>
2023-12-23 00:48:42 +0300

    It writes and reads the history from file!

--- spanish_tutor.py
+++ spanish_tutor.py
@@ -4,21 +4,30 @@ import os
 genai.configure(api_key="AIzaSyAq9duOY8_UAeZAJcKdptEI5PDZstlgqP8")
 
 model = genai.GenerativeModel('gemini-pro')
-chat = model.start_chat()
+file = open("chat_history.txt", "r")
+from_file = file.read()
+file.close()
+print(from_file)
+chat = model.start_chat(history=[])
 
 response = chat.send_message("Hello.")
 print("User: hello")
 print("Gemini: " + response.text)
 
+chat_history=[f"User: Hello. Response: {response.text}"]
 cond = True
 
 while(cond):
     user_input = input("User: ")
-    response = chat.send_message(user_input)
+    response = chat.send_message(user_input, generation_config={'temperature': 0.0})
     print("Gemini: " + response.text)
+    chat_history.append("User: " + user_input + " Response: " + response.text)
     if(user_input == "exit"):
-        history = chat.history
         cond = False
-
-print(history[0])
+    
+print(chat_history)
+file = open("chat_history.txt", "a")
+for message in chat_history:
+    file.write(f"\n{message}")
+file.close
         
