4acd778738d58a7cb2e68853da9b3337d1c30635
Mustafa Tolga Akbaba <76005690+mtlgkbb20@users.noreply.github.com>
2025-09-02 11:34:31 +0300

    Update KpiDefinition.java

--- KpiDefinition.java
+++ KpiDefinition.java
@@ -10,14 +10,4 @@ public class KpiDefinition {
     private String name;
     private String query; // örnek: approval_time > 48
     private String owner;
-
-    public Long getId() { return id; }
-    public String getName() { return name; }
-    public String getQuery() { return query; }
-    public String getOwner() { return owner; }
-
-    public void setId(Long id) { this.id = id; }
-    public void setName(String name) { this.name = name; }
-    public void setQuery(String query) { this.query = query; }
-    public void setOwner(String owner) { this.owner = owner; }
 }
